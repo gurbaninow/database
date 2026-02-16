1f7c478a97a7ae5bca5709fc1f60d2cbc0aca381
Sarabveer Singh <me@sarabveer.me>
2026-02-15 19:49:45 -0500

    build: update to mariadb 12, fix backport build

--- docker/mariadb/build_db.sh
+++ docker/mariadb/build_db.sh
@@ -3,36 +3,36 @@
 set -eo pipefail
 
 # Install the database
-echo "Installing MySQL"
-mysql_install_db
+echo "Installing MariaDB"
+mariadb-install-db
 
-# Start the MySQL daemon in the background.
-echo "Starting MySQL daemon"
-mysqld --user=root &
-mysql_pid=$!
+# Start the MariaDB daemon in the background.
+echo "Starting MariaDB daemon"
+mariadbd --user=root &
+mariadb_pid=$!
 
 echo "Waiting for database to come up"
-until mysqladmin ping >/dev/null 2>&1; do
+until mariadb-admin ping >/dev/null 2>&1; do
   echo -n "."; sleep 0.2
 done
 
-echo "Removing MySQL default users"
-mysql --protocol=socket -h localhost -u root -e "DELETE FROM mysql.user WHERE user=''"
-echo "Creating shabados MySQL user"
-mysql --protocol=socket -h localhost -u root -e "CREATE USER 'shabados'@'%'"
-echo "Creating shabad-os database"
-mysql --protocol=socket -h localhost -u root -e "CREATE DATABASE \`shabados\`"
-echo "Granting priviledges to shabados database"
-mysql --protocol=socket -h localhost -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'shabados'@'%'"
-mysql --protocol=socket -h localhost -u root -e "FLUSH PRIVILEGES"
+echo "Removing MariaDB default users"
+mariadb --protocol=socket -h localhost -u root -e "DELETE FROM mysql.user WHERE user=''"
+echo "Creating gurbaninow MariaDB user"
+mariadb --protocol=socket -h localhost -u root -e "CREATE USER 'gurbaninow'@'%'"
+echo "Creating gurbaninow database"
+mariadb --protocol=socket -h localhost -u root -e "CREATE DATABASE \`gurbaninow\`"
+echo "Granting priviledges to gurbaninow database"
+mariadb --protocol=socket -h localhost -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'gurbaninow'@'%'"
+mariadb --protocol=socket -h localhost -u root -e "FLUSH PRIVILEGES"
 
 # Populate the database with data
 echo "Populating data"
 npm run build-mysql
 
-# Tell the MySQL daemon to shutdown.
-echo "Shutting down MySQL"
-mysqladmin shutdown
+# Tell the MariaDB daemon to shutdown.
+echo "Shutting down MariaDB"
+mariadb-admin shutdown
 
-# Wait for the MySQL daemon to exit.
-wait $mysql_pid
+# Wait for the MariaDB daemon to exit.
+wait $mariadb_pid
--- docker/mariadb/knexfile.js
+++ docker/mariadb/knexfile.js
@@ -2,8 +2,8 @@ const config = {
   client: 'mysql2',
   connection: {
     host: 'localhost',
-    user: 'shabados',
-    database: 'shabados',
+    user: 'gurbaninow',
+    database: 'gurbaninow',
   },
   useNullAsDefault: true,
 }
